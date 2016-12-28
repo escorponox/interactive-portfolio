09ffae46374b8170c31f2386db18342916cc245b
Carlos Coves Prieto <escorponox@gmail.com>
2016-12-28 13:30:59 +0100

    all scoll effects working

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 import smoothScroll from './smooth-scroll';
+import throttle from 'lodash.throttle';
 import signUp from './signup-form';
 import questionnaire from './questionnaire';
 import color from './color';
@@ -9,4 +10,77 @@ documentReady(questionnaire);
 documentReady(signUp);
 documentReady(color);
 documentReady(calculator);
-documentReady(smoothScroll);
+
+documentReady(() => {
+  let previousScroll = 0;
+  const scrollToTopButton = document.getElementById('scroll-to-top');
+  const header = document.querySelector('header');
+  const menuInput = document.getElementById('menu-toggle');
+  const subMenuInput = document.getElementById('sub-menu-toggle');
+
+  const showScrollToTopButton = throttle(() => {
+    window.pageYOffset > 200
+      ? scrollToTopButton.classList.add('c-to-top--show')
+      : scrollToTopButton.classList.remove('c-to-top--show');
+  }, 200);
+
+  const hideSideBar = throttle((event) => {
+    menuInput.checked = false;
+    subMenuInput.checked = false;
+  }, 200);
+
+  const showHideHeader = throttle(() => {
+    const currentScroll = window.pageYOffset;
+    currentScroll - previousScroll > 0
+      ? header.classList.add('header--hide')
+      : header.classList.remove('header--hide');
+    previousScroll = currentScroll;
+  }, 200);
+
+  window.addEventListener('scroll', showScrollToTopButton);
+  window.addEventListener('scroll', hideSideBar);
+  window.addEventListener('scroll', showHideHeader);
+  scrollToTopButton.addEventListener('click', smoothScroll.bind(null, 500, 0, 0));
+
+  [].forEach.call(document.querySelectorAll('.section-link'), (link) => {
+    link.addEventListener('click', function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      const linkHref = event.currentTarget.getAttribute('href');
+      const target = document.querySelector(linkHref);
+      smoothScroll(500, target.offsetTop, header.offsetHeight);
+    })
+  });
+
+  [].forEach.call(document.querySelectorAll('label[for=menu-toggle]'), (label) => {
+    label.addEventListener('click', () => {
+      subMenuInput.checked = false;
+    })
+  });
+
+  [].forEach.call(document.querySelectorAll('label.accordion-label'), (label) => {
+    label.addEventListener('click', function (event) {
+      event.stopPropagation();
+      const forInput = document.querySelector('#' + event.currentTarget.getAttribute('for'));
+      if (!forInput.checked) {
+        const target = event.currentTarget;
+        setTimeout(function () {
+          smoothScroll(500, target.offsetTop, header.offsetHeight);
+        }, 500);
+      }
+    })
+  });
+
+  [].forEach.call(document.querySelectorAll('label.accordion-label-menu'), (label) => {
+    label.addEventListener('click', function (event) {
+      event.stopPropagation();
+      const labelFor = event.currentTarget.getAttribute('for');
+      const target = document.querySelector('label.accordion-label[for=' + labelFor + ']');
+      setTimeout(function () {
+        smoothScroll(500, target.offsetTop, header.offsetHeight);
+      }, 500);
+
+    })
+  });
+
+});
--- src/scripts/smooth-scroll.js
+++ src/scripts/smooth-scroll.js
@@ -1,92 +1,28 @@
-import throttle from 'lodash.throttle';
+export default (duration, endScroll, offset) => {
+  let start = null;
+  const initScroll = window.pageYOffset;
+  offset = endScroll - initScroll < 0 ? offset || 0 : 0;
+  console.log(offset);
 
-const smoothScrolling = () => {
-
-  const scrollToTopButton = document.getElementById('scroll-to-top');
-  const header = document.querySelector('header');
-
-  window.addEventListener('scroll', throttle(showScrollToTopButton, 50));
-  scrollToTopButton.addEventListener('click', smoothScroll.bind(null, 500, 0, 0));
-
-  [].forEach.call(document.querySelectorAll('a.section-link'), (link) => {
-    link.addEventListener('click', function (event) {
-      event.stopPropagation();
-      event.preventDefault();
-      const linkHref = event.currentTarget.getAttribute('href');
-      const target = document.querySelector(linkHref);
-      smoothScroll(500, target.offsetTop);
-    })
-  });
-
-  [].forEach.call(document.querySelectorAll('label.section-link'), (link) => {
-    link.addEventListener('click', function (event) {
-      event.stopPropagation();
-      const linkHref = event.currentTarget.getAttribute('href');
-      const target = document.querySelector(linkHref);
-      const forInput = document.querySelector('#' + event.currentTarget.getAttribute('for'));
-      if (!forInput.checked) {
-        smoothScroll(500, target.offsetTop);
-      }
-    })
-  });
-
-  [].forEach.call(document.querySelectorAll('label.accordion-label'), (label) => {
-    label.addEventListener('click', function (event) {
-      event.stopPropagation();
-      const forInput = document.querySelector('#' + event.currentTarget.getAttribute('for'));
-      if (!forInput.checked) {
-        const target = event.currentTarget;
-        setTimeout(function () {
-          smoothScroll(500, target.offsetTop, header.offsetHeight);
-        }, 500);
-      }
-    })
-  });
-
-  [].forEach.call(document.querySelectorAll('label.accordion-label-menu'), (label) => {
-    label.addEventListener('click', function (event) {
-      event.stopPropagation();
-      const labelFor = event.currentTarget.getAttribute('for');
-      const target = document.querySelector('label.accordion-label[for=' + labelFor + ']');
-      setTimeout(function () {
-        smoothScroll(500, target.offsetTop, header.offsetHeight);
-      }, 500);
-
-    })
-  });
-
-  function showScrollToTopButton() {
-    window.pageYOffset > 200 ? scrollToTopButton.classList.add('c-to-top--show') : scrollToTopButton.classList.remove('c-to-top--show');
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easing(progress, initScroll, endScroll - offset - initScroll, duration));
+    if (progress < duration) {
+      window.requestAnimationFrame(step);
+    } else {
+      endAnimation();
+    }
   }
 
-  function smoothScroll(duration, endScroll, offset) {
-    let start = null;
-    const initScroll = window.pageYOffset;
-    offset = offset || 0;
-
-    function step(timestamp) {
-      if (!start) start = timestamp;
-      const progress = timestamp - start;
-      window.scrollTo(0, easing(progress, initScroll, endScroll - offset - initScroll, duration));
-      if (progress < duration) {
-        window.requestAnimationFrame(step);
-      } else {
-        endAnimation();
-      }
-    }
-
-    // Easein Cubic - George McGinley Smith - https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
-    function easing(t, b, c, d) {
-      return c * (t /= d) * t * t + b;
-    }
-
-    function endAnimation() {
-      window.scrollTo(0, endScroll - offset);
-    }
-
-    window.requestAnimationFrame(step);
+  // Easein Cubic - George McGinley Smith - https://github.com/danro/jquery-easing/blob/master/jquery.easing.js
+  function easing(t, b, c, d) {
+    return c * (t /= d) * t * t + b;
   }
 
+  function endAnimation() {
+    window.scrollTo(0, endScroll - offset);
+  }
+
+  window.requestAnimationFrame(step);
 };
-
-export default smoothScrolling;
