5af9e5b17ac2c33db726627897e7a334e5df6e82
Carlos Coves Prieto <escorponox@gmail.com>
2017-02-21 01:28:08 +0100

    Update 2017-02-21T00:28:05.732Z

--- README.md
+++
@@ -1,4 +0,0 @@
-# interactive-portfolio
-Interactive Portfolio
-
-Migrated to [Frontend Init](https://github.com/cusxio/frontend-init)
--- src/scripts/menu-highlight.js
+++
@@ -1,3 +0,0 @@
-export default (sections) => Array.from(sections).reverse().find(
-  (section) => section.offsetTop < window.pageYOffset + (window.innerHeight / 2)
-).getAttribute('id')
